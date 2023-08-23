1012a42825a098671d6dd4689a9fd4b58de88711
dhairya-07 <dhairya3208@gmail.com>
2023-08-23 22:34:47 +0530

    Tweet editing handled

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -8,7 +8,11 @@ const {
   editTweet,
   deleteTweet,
 } = require('../controller/tweetController');
-const { create_tweet, get_tweet } = require('../controller/renderPage');
+const {
+  create_tweet,
+  get_tweet,
+  edit_tweet,
+} = require('../controller/renderPage');
 
 const router = Router();
 
@@ -17,6 +21,9 @@ router.get('/my-tweets', protect, getMyTweets);
 router.get('/', getAllTweets);
 
 router.route('/create').get(protect, create_tweet).post(protect, createTweet);
+
+router.route('/:id/edit').get(protect, edit_tweet);
+
 router
   .route('/:id')
   .get(protect, get_tweet)

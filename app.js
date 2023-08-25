be5d88297fa8716f7a358e24646803fd286fcd59
dhairya-07 <dhairya3208@gmail.com>
2023-08-25 15:21:25 +0530

    Getting tweets query made better

--- app.js
+++ app.js
@@ -50,7 +50,10 @@ app.get(
     const users = await Users.find({});
     // console.log(req.user.following);
     const tweets = await Tweets.find({
-      createdBy: { $in: req.user.following },
+      $or: [
+        { createdBy: { $in: req.user.following } },
+        { createdBy: req.user.id },
+      ],
     });
     console.log(tweets);
     return res.render('home', {
